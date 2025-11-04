c4e895b1a5986e1a5adb55023e4d935651816b24
Denis Surmanidze <surmanidzedenis609@gmail.com>
2025-11-04 18:01:44 +0300

    fixed ocelot, docker compose

--- NexusPaySolution/api-gateway/src/API.Gateway/Program.cs
+++ NexusPaySolution/api-gateway/src/API.Gateway/Program.cs
@@ -36,32 +36,32 @@ builder.Services.AddAuthorization(options =>
     options.AddPolicy("UserOrAdmin", policy => policy.RequireRole("User", "Admin"));
 });
 
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.SwaggerDoc("identity", new OpenApiInfo { Title = "Identity Service", Version = "v1" });
-//    c.SwaggerDoc("logging", new OpenApiInfo { Title = "Logging Service", Version = "v1" });
-//    c.SwaggerDoc("notifications", new OpenApiInfo { Title = "Notification Service", Version = "v1" });
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("identity", new OpenApiInfo { Title = "Identity Service", Version = "v1" });
+    c.SwaggerDoc("logging", new OpenApiInfo { Title = "Logging Service", Version = "v1" });
+    c.SwaggerDoc("notifications", new OpenApiInfo { Title = "Notification Service", Version = "v1" });
 
-//    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-//    {
-//        Description = "JWT Authorization header using the Bearer scheme",
-//        Name = "Authorization",
-//        In = ParameterLocation.Header,
-//        Type = SecuritySchemeType.ApiKey,
-//        Scheme = "Bearer"
-//    });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
 
-//    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-//    {
-//        {
-//            new OpenApiSecurityScheme
-//            {
-//                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
-//            },
-//            Array.Empty<string>()
-//        }
-//    });
-//});
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 builder.Services.AddOcelot(builder.Configuration);
 builder.Services.AddCors(options =>
@@ -74,14 +74,14 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    //app.UseSwagger();
-    //app.UseSwaggerUI(c =>
-    //{
-    //    c.SwaggerEndpoint("/identity/swagger/v1/swagger.json", "Identity Service v1");
-    //    c.SwaggerEndpoint("/logging/swagger/v1/swagger.json", "Logging Service v1");
-    //    c.SwaggerEndpoint("/notifications/swagger/v1/swagger.json", "Notification Service v1");
-    //    c.RoutePrefix = "docs";
-    //});
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/identity/swagger/v1/swagger.json", "Identity Service v1");
+        c.SwaggerEndpoint("/logging/swagger/v1/swagger.json", "Logging Service v1");
+        c.SwaggerEndpoint("/notifications/swagger/v1/swagger.json", "Notification Service v1");
+        c.RoutePrefix = "docs";
+    });
 }
 
 app.UseCors("AllowAll");
