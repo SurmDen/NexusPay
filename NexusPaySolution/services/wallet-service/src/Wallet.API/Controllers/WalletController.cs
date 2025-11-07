3f1c1d385465b94848444ce7cb11845e033dec3f
Denis Surmanidze <surmanidzedenis609@gmail.com>
2025-11-07 18:27:49 +0300

    completed wallet microservice

---
+++ NexusPaySolution/services/wallet-service/src/Wallet.API/Controllers/WalletController.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Wallet.Application.Interfaces;
+using Wallet.Application.Wallet.Queries;
+using Wallet.Domain.Exceptions;
+
+namespace Wallet.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/wallet")]
+    public class WalletController : ControllerBase
+    {
+        public WalletController(IMediator mediator, ILoggerService logger)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        private IMediator _mediator;
+        private ILoggerService _logger;
+
+        [HttpGet("balance/get/userid/{id:guid}")]
+        public async Task<IActionResult> GetBalanceAsync(Guid userId)
+        {
+            try
+            {
+                GetBalanceQuery query = new GetBalanceQuery()
+                {
+                    UserId = userId
+                };
+
+                decimal walletBalance = await _mediator.Send(query);
+
+                return Ok(new { balance = walletBalance, code = 200 });
+            }
+            catch(NotFoundException e)
+            {
+                return BadRequest(new {code = 400, message = e.Message});
+            }
+            catch (Exception e)
+            {
+                await _logger.LogError(e.Message, "WalletController.GetBalanceAsync", e.GetType().FullName);
+
+                return Problem(statusCode: 500, title: "Server error", detail: "Error occured while try to get balance");
+            }
+        }
+    }
+}
